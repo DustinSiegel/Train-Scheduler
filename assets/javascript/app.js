29a9e260151c3dec428f78a71fc67dc9e86795ee
Dustin J. Siegel <dustinsdirectory@Dustins-MacBook-Pro.local>
2017-06-24 00:24:08 -0600

    Firebase updates with the most recent entry, need to make them persist

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,15 @@
 // FireBase Link =======================================================================================================
 
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyDms5AHA1Q__tuwAbyrwJVo3krAEoqMI6E",
-  //   authDomain: "train-scheduler-7f64d.firebaseapp.com",
-  //   databaseURL: "https://train-scheduler-7f64d.firebaseio.com",
-  //   projectId: "train-scheduler-7f64d",
-  //   storageBucket: "train-scheduler-7f64d.appspot.com",
-  //   messagingSenderId: "130680246488"
-  // };
-  // firebase.initializeApp(config);
-
+  var config = {
+    apiKey: "AIzaSyDms5AHA1Q__tuwAbyrwJVo3krAEoqMI6E",
+    authDomain: "train-scheduler-7f64d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7f64d.firebaseio.com",
+    projectId: "train-scheduler-7f64d",
+    storageBucket: "train-scheduler-7f64d.appspot.com",
+    messagingSenderId: "130680246488"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
 
 // Global Variables ====================================================================================================
 
@@ -36,40 +35,33 @@ $(document).on("click", "#searchBtn", function() {
     destination = $("#destination").val().trim();
     trainTime = $("#trainTime").val();
     frequency = $("#frequency").val();
-    // Add in the Search Term
-    // var newURL = queryURLBase + "&q=" + trainName;
+    
+     var trainSchedule = {trainName, destination, trainTime, frequency}
+    database.ref().set(trainSchedule);
+
     console.log(trainName);
     console.log(destination);
     console.log(trainTime);
     console.log(frequency);
 
-
-
-
-
-
 //Moment.js Variables ================================================================================================    
 
-    var firstTimeConverted = moment(trainTime, "hh:mm");
-    console.log("First Time: " + firstTimeConverted);
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-    // Next Train
-    nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    var firstTimeConverted = moment(trainTime, "hh:mm");    // First train time converted to miltary time
+    var currentTime = moment();    // Current Time
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");    // Difference between the times
+    var tRemainder = diffTime % frequency;    // Time apart (remainder)
+    var tMinutesTillTrain = frequency - tRemainder;     // Minute Until Train
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");    // Next Train
+
 
 $("#wellSection").append("<h5 class='col-lg-2'>" + trainName + "</h5><h5 class='col-lg-3'>" + destination + "</h5><h5 class='col-lg-3'>" + trainTime + " Hours</h5><h5 class='col-lg-2'>"+ frequency + " Minutes</h5><h5 class='col-lg-2'>" + moment(nextTrain).format("hh:mm") + "</h5>");
-
+    
+    console.log("First Time: " + firstTimeConverted);
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    console.log("REMAINDER: " + tRemainder);
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
         
     return false;
 });
