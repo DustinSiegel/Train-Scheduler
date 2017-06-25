ce8a4743cc3742d431341e0c971465d1e82f522d
Dustin J. Siegel <dustinsdirectory@Dustins-MacBook-Pro.local>
2017-06-24 21:33:26 -0600

    Getting a callback from firebase but moment.js no longer functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 // FireBase Link =======================================================================================================
 
-  var config = {
+var config = {
     apiKey: "AIzaSyDms5AHA1Q__tuwAbyrwJVo3krAEoqMI6E",
     authDomain: "train-scheduler-7f64d.firebaseapp.com",
     databaseURL: "https://train-scheduler-7f64d.firebaseio.com",
     projectId: "train-scheduler-7f64d",
     storageBucket: "train-scheduler-7f64d.appspot.com",
     messagingSenderId: "130680246488"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
 // Global Variables ====================================================================================================
 
@@ -20,14 +20,26 @@ var frequency   = "";
 var nextTrain   = "";
 var firebase;
 
-// Functions and Processes ===========================================================================================================
+// //Moment.js Variables ================================================================================================    
 
-// database.ref().on("value", function (snapshot) {
-//     trainSchedule = snapshot.val().trianSchedule;
-//     $("#wellSection").append("<h5 class='col-lg-2'>" + trainName + "</h5><h5 class='col-lg-3'>" + destination + "</h5><h5 class='col-lg-3'>" + trainTime + " Hours</h5><h5 class='col-lg-2'>"+ frequency + " Minutes</h5><h5 class='col-lg-2'>" + moment(nextTrain).format("hh:mm") + "</h5>");
-//     }, function (errorObject) {
-//         console.log("The read failed: " + errorObject.code);
-// });
+function timeCalculator() {
+    
+    var firstTimeConverted = moment(trainTime, "hh:mm");    // First train time converted to miltary time
+    var currentTime = moment();    // Current Time
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");    // Difference between the times
+    var tRemainder = diffTime % frequency;    // Time apart (remainder)
+    var tMinutesTillTrain = frequency - tRemainder;     // Minute Until Train
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");    // Next Train
+    
+    console.log("First Time: " + firstTimeConverted);
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    console.log("REMAINDER: " + tRemainder);
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));       
+};
+
+// Functions and Processes ===========================================================================================================
 
 $(document).on("click", "#searchBtn", function() {
 
@@ -35,33 +47,30 @@ $(document).on("click", "#searchBtn", function() {
     destination = $("#destination").val().trim();
     trainTime = $("#trainTime").val();
     frequency = $("#frequency").val();
-    
-    var trainSchedule = {trainName, destination, trainTime, frequency}
-    database.ref().set(trainSchedule);
 
-    console.log(trainName);
-    console.log(destination);
-    console.log(trainTime);
-    console.log(frequency);
+    timeCalculator();
 
-//Moment.js Variables ================================================================================================    
+    var trainSchedule = {"trainName": trainName, "destination": destination, "trainTime": trainTime, "frequency": frequency};
+    database.ref("trains/").set(trainSchedule);
 
-    var firstTimeConverted = moment(trainTime, "hh:mm");    // First train time converted to miltary time
-    var currentTime = moment();    // Current Time
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");    // Difference between the times
-    var tRemainder = diffTime % frequency;    // Time apart (remainder)
-    var tMinutesTillTrain = frequency - tRemainder;     // Minute Until Train
-    nextTrain = moment().add(tMinutesTillTrain, "minutes");    // Next Train
-
-
-$("#wellSection").append("<h5 class='col-lg-2'>" + trainName + "</h5><h5 class='col-lg-3'>" + destination + "</h5><h5 class='col-lg-3'>" + trainTime + " Hours</h5><h5 class='col-lg-2'>"+ frequency + " Minutes</h5><h5 class='col-lg-2'>" + moment(nextTrain).format("hh:mm") + "</h5>");
-    
-    console.log("First Time: " + firstTimeConverted);
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-    console.log("REMAINDER: " + tRemainder);
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-        
-    return false;
+    $("#wellSection").append("<h5 class='col-lg-2'>" + snapshot.val().trainName + "</h5><h5 class='col-lg-3'>" + snapshot.val().destination + "</h5><h5 class='col-lg-3'>" + snapshot.val().trainTime + " Hours</h5><h5 class='col-lg-2'>"+ snapshot.val().frequency + " Minutes</h5><h5 class='col-lg-2'>" + moment(nextTrain).format("hh:mm") + "</h5>");
 });
+
+$(document).ready(function() {
+
+    database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+
+    timeCalculator();
+
+    $("#wellSection").append("<h5 class='col-lg-2'>" + snapshot.val().trainName + "</h5><h5 class='col-lg-3'>" + snapshot.val().destination + "</h5><h5 class='col-lg-3'>" + snapshot.val().trainTime + " Hours</h5><h5 class='col-lg-2'>"+ snapshot.val().frequency + " Minutes</h5><h5 class='col-lg-2'>" + moment(nextTrain).format("hh:mm") + "</h5>");
+    });
+
+    database.ref().on("value", function (snapshot) {
+        trainSchedule = snapshot.val().trianSchedule;
+       }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+        return false;
+});
+
